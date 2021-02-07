76746cb8be996627cb138d1af4d4ac6e0148393f
YusukeNaito08 <yusuke0208na@gmail.com>
2021-02-07 03:00:30 +0000

    [FIX]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  after_create :send_thanks_mail
-  
-  def send_thanks_mail
-    NotificationMailer.thanks_mail(self).deliver
-  end  
-
+ 
 
   has_many :books, dependent: :destroy
   attachment :profile_image
